b7286c925b141d659b10408af70decd86a05fb03
Vijay Bahadur <120042462+guptavijay119@users.noreply.github.com>
2024-02-10 22:19:23 +0530

    commit done for tp.

---
+++ src/main/java/com/example/corejava/ForEach.java
@@ -0,0 +1,17 @@
+package com.example.corejava;
+
+public class ForEach {
+
+	public static void main(String[] args) {
+		
+		
+		int array[]= {19,20,21,22,23,24,25};
+		
+		for(int element :array) {
+			System.out.println(element+"printing of element");
+			
+		}
+		
+	}
+
+}
